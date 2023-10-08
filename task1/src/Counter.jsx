2a08fa104891fa96dd99db1469bc24f2cf162f7b
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-08 21:38:00 +0300

    counter -react hooks

--- task1/src/Counter.jsx
+++ task1/src/Counter.jsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="counter">
-      <button
-        className="counter__button"
-        onClick={() => setCounter(counter - 1)}
-      >
+      <button className="counter__button" onClick={decrement}>
         -
       </button>
       <span className="counter__value" onClick={() => setCounter(0)}>
-        {' '}
-        {counter}{' '}
+        {counter}
       </span>
       <button
         className="counter__button"
