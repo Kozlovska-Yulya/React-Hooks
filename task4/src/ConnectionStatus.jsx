d5503b2157a28affbc068ce0705febcb0394118c
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-10 00:01:25 +0300

    online/offline status

--- task4/src/ConnectionStatus.jsx
+++ task4/src/ConnectionStatus.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const ConnectionStatus = () => {
-  const [status, setStatus] = useState(navigator.onLine ? 'online' : 'offline');
+  const [status, setStatus] = useState('online');
 
   useEffect(() => {
     const handleOnline = () => {
