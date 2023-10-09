359ffcea6e0bffb2414f1e3cca82696e004fff82
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-10 00:03:43 +0300

    online/offline status

--- task4/src/ConnectionStatus.jsx
+++ task4/src/ConnectionStatus.jsx
@@ -4,19 +4,13 @@ const ConnectionStatus = () => {
   const [status, setStatus] = useState('online');
 
   useEffect(() => {
-    const handleOnline = () => {
-      setStatus('online');
-    };
-
     const handleOffline = () => {
       setStatus('offline');
     };
 
-    window.addEventListener('online', handleOnline);
     window.addEventListener('offline', handleOffline);
 
     return () => {
-      window.removeEventListener('online', handleOnline);
       window.removeEventListener('offline', handleOffline);
     };
   }, []);
